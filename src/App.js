9f95a0ee7d0ca1efffdefa729ebe440c11c2eff4
kyle <kylecalbert@gmail.com>
2021-02-27 15:50:31 +0000

    got the weather app api call working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { WiDayCloudyWindy } from "react-icons/wi";
 import { FaSearch } from "react-icons/fa";
 
 function App() {
-  const api = {
-    key: "bd1e10f886aa95086bf83c63247820af",
-    base: "https://home.openweathermap.org/data/2.5",
+  const [search, setSearch] = useState("");
+  const [weather, setWeather] = useState([]);
+
+  const API_KEY = "bd1e10f886aa95086bf83c63247820af";
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`;
+
+  const getWeather = async () => {
+    console.log("we are here first");
+    // const data = await fetch(WEATHER_API).then((response) => response.json()
+    const response = await fetch(WEATHER_API);
+    const data = await response.json();
+    setWeather(data);
+    console.log(data);
+  };
+
+  const handleOnChange = (e) => {
+    setSearch(e.target.value);
+    e.preventDefault();
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    getWeather();
   };
   return (
     <div className="app-container">
@@ -18,22 +38,31 @@ function App() {
           <WiDayCloudyWindy className="icon fav" size="5rem" />
         </div>
       </div>
-
       <div className="search-nav">
-        <input className="searchbar" type="text" />
-        <button className="search-button" type="submit">
-          <FaSearch className="search-icon" size="1rem" />
-        </button>
+        <form onSubmit={handleOnSubmit}>
+          <input
+            className="searchbar"
+            type="text"
+            onChange={handleOnChange}
+            value={search}
+          />
+          <button className="search-button" type="submit">
+            <FaSearch className="search-icon" size="1rem" />
+          </button>
+        </form>
       </div>
 
       <div className="content">
         <div className="weather-info">
           <div className="general-details">
-            <div className="weather-location">N/A</div>
-            <div className="weather-status">N/A</div>
+            <div className="weather-location">London</div>
+
+            <div className="temp-box">
+              <div className="temp">23c</div>
+            </div>
             <div className="weather-icon">N/A</div>
-            <div className="weather-description">N/A</div>
-            <div className="weather-date">N/A</div>
+            <div className="weather-description">Sunny</div>
+            <div className="weather-date">27/2/21</div>
           </div>
           <div className="specific-details">
             <div className="detail">
