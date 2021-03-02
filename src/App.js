79cecb32d7ff034b46501ea67627fd15f4482888
kyle <kylecalbert@gmail.com>
2021-03-02 13:20:56 +0000

    completed weather-app

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [weather, setWeather] = useState([]);
 
   const API_KEY = "bd1e10f886aa95086bf83c63247820af";
-  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=${API_KEY}`;
+  const WEATHER_API = `https://api.openweathermap.org/data/2.5/weather?q=${search}&mode=json&units=metric&appid=${API_KEY}`;
 
   const getWeather = async () => {
     console.log("we are here first");
@@ -53,33 +53,42 @@ function App() {
       </div>
 
       <div className="content">
-        <div className="weather-info">
-          <div className="general-details">
-            <div className="weather-location">London</div>
+        {typeof weather.main != "undefined" ? (
+          <div className="weather-info">
+            <div className="general-details">
+              <div className="weather-location">{weather.name}</div>
+              <div className="temp-box">
+                <div className="temp"> {Math.round(weather.main.temp)}°</div>
+              </div>
+              <div className="weather-icon">
+                <img
+                  src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                ></img>
+              </div>
+              <div className="weather-description">
+                {weather.weather[0].description}
+              </div>
+            </div>
 
-            <div className="temp-box">
-              <div className="temp">23c</div>
-            </div>
-            <div className="weather-icon">N/A</div>
-            <div className="weather-description">Sunny</div>
-            <div className="weather-date">27/2/21</div>
-          </div>
-          <div className="specific-details">
-            <div className="detail">
-              <div className="title">Wind</div>
-              <div className="value">TBD</div>
-            </div>
+            <div className="specific-details">
+              <div className="detail">
+                <div className="title">Wind</div>
+                <div className="value">{weather.wind.speed}</div>
+              </div>
 
-            <div className="detail">
-              <div className="title">Temp</div>
-              <div className="value">TBD</div>
-            </div>
-            <div className="detail">
-              <div className="title">Percipitation</div>
-              <div className="value">TBD</div>
+              <div className="detail">
+                <div className="title">Humidity</div>
+                <div className="value">{weather.main.humidity}</div>
+              </div>
+              <div className="detail">
+                <div className="title">Pressure</div>
+                <div className="value">{weather.main.pressure}</div>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          ""
+        )}
       </div>
     </div>
   );
